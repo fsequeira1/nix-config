d4ff7a58b6176bcb6fa8d7a15d120ed6113dbd08
Filipe Sequeira <f_sequeira994@hotmail.com>
2024-03-02 02:28:49 +0000

    reduce 7W on consumption

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -48,46 +48,46 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-  # Enable OpenGL
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
-
-  services.xserver.videoDrivers = ["nvidia"];
-  hardware.nvidia = {
-    package = config.boot.kernelPackages.nvidiaPackages.production; # (installs 535)
-    modesetting.enable = true;
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-    # of just the bare essentials.
-    powerManagement.enable = false;
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = true;
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
-
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    prime = {
-      #sync.enable = true;
-      offload = {
-        enable = true;
-        enableOffloadCmd = true;
-      };
-      nvidiaBusId = "PCI:01:00:0"; # Found with lspci | grep VGA
-      intelBusId = "PCI:00:02:0"; # Found with lspci | grep VGA
-    };
-  };
+#  # Enable OpenGL
+#  hardware.opengl = {
+#    enable = true;
+#    driSupport = true;
+#    driSupport32Bit = true;
+#  };
+#
+#  services.xserver.videoDrivers = ["nvidia"];
+#  hardware.nvidia = {
+#    package = config.boot.kernelPackages.nvidiaPackages.production; # (installs 535)
+#    modesetting.enable = true;
+#    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+#    # Enable this if you have graphical corruption issues or application crashes after waking
+#    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+#    # of just the bare essentials.
+#    powerManagement.enable = false;
+#    # Fine-grained power management. Turns off GPU when not in use.
+#    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+#    powerManagement.finegrained = true;
+#    # Use the NVidia open source kernel module (not to be confused with the
+#    # independent third-party "nouveau" open source driver).
+#    # Support is limited to the Turing and later architectures. Full list of
+#    # supported GPUs is at:
+#    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+#    # Only available from driver 515.43.04+
+#    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+#    open = false;
+#
+#    # Enable the Nvidia settings menu,
+#    # accessible via `nvidia-settings`.
+#    nvidiaSettings = true;
+#
+#    prime = {
+#      #sync.enable = true;
+#      offload = {
+#        enable = true;
+#        enableOffloadCmd = true;
+#      };
+#      nvidiaBusId = "PCI:01:00:0"; # Found with lspci | grep VGA
+#      intelBusId = "PCI:00:02:0"; # Found with lspci | grep VGA
+#    };
+#  };
 }
