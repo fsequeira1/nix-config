3e439d79d1390fe43d1a19f923b61ae22bc5e301
Filipe Sequeira <fsequeira1@users.noreply.github.com>
2024-03-14 15:53:12 +0000

    Update hardware-configuration.nix

--- hosts/hornet/hardware-configuration.nix
+++ hosts/hornet/hardware-configuration.nix
@@ -38,6 +38,7 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
+  services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
     # Modesetting is required.
     modesetting.enable = true;
