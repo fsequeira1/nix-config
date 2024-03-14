3dbaa2bf27ac372a5d50742e7a768e46abdcb90d
Filipe Sequeira <fsequeira1@users.noreply.github.com>
2024-03-14 14:29:58 +0000

    Update configuration.nix

--- hosts/hornet/configuration.nix
+++ hosts/hornet/configuration.nix
@@ -17,6 +17,5 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.initrd.luks.devices."luks-1ba7599a-59b8-429c-964f-36a755b5a75d".device = "/dev/disk/by-uuid/1ba7599a-59b8-429c-964f-36a755b5a75d";
   networking.hostName = "hornet"; # Define your hostname.
 }
