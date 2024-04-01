9654dd36c74ae8797c9ab97bfb3c9d53f6cb8afd
Filipe Sequeira <fsequeira1@users.noreply.github.com>
2024-04-01 23:00:35 +0100

    increase cpu on battery to avoid throttle

--- modules/nixos/laptop.nix
+++ modules/nixos/laptop.nix
@@ -24,7 +24,7 @@
         CPU_MIN_PERF_ON_AC = 0;
         CPU_MAX_PERF_ON_AC = 100;
         CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 20;
+        CPU_MAX_PERF_ON_BAT = 35;
 
         #Optional helps save long term battery health
         START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
